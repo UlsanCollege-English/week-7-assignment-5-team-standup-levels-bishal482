f156f030ead7f1ffc21207328435ba13165de579
bishal482 <galanbishal20@gmail.com>
2025-10-28 15:44:46 +0900

    Going home haha

--- src/levels.py
+++ src/levels.py
@@ -1,3 +1,5 @@
+# levels.py
+
 from collections import deque
 
 class Node:
@@ -7,13 +9,59 @@ class Node:
         self.right = right
 
 def level_order(root):
-    # TODO: return list of lists
-    pass
+    """Return a list of levels in the binary tree (standard BFS)."""
+    if not root:
+        return []
+    result = []
+    queue = deque([root])
+    while queue:
+        level = []
+        for _ in range(len(queue)):
+            node = queue.popleft()
+            level.append(node.val)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        result.append(level)
+    return result
 
 def zigzag_level_order(root):
-    # TODO
-    pass
+    """Return a list of levels in zigzag order (alternate left-right)."""
+    if not root:
+        return []
+    result = []
+    queue = deque([root])
+    left_to_right = True
+    while queue:
+        level = []
+        for _ in range(len(queue)):
+            node = queue.popleft()
+            level.append(node.val)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        if not left_to_right:
+            level.reverse()
+        result.append(level)
+        left_to_right = not left_to_right
+    return result
 
 def right_side_view(root):
-    # TODO
-    pass
+    """Return a list of rightmost values at each level."""
+    if not root:
+        return []
+    view = []
+    queue = deque([root])
+    while queue:
+        level_len = len(queue)
+        for i in range(level_len):
+            node = queue.popleft()
+            if i == level_len - 1:
+                view.append(node.val)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+    return view
